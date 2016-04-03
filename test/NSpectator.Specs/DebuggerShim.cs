1f5cad1dff8043ffaed3971347a731fac92cf303
jonny-novikov <oneillcps@gmail.com>
2016-04-03 17:43:05 +0300

    Improved partial DebuggerShim ready to inherit

--- test/NSpectator.Specs/DebuggerShim.cs
+++ test/NSpectator.Specs/DebuggerShim.cs
@@ -1,40 +1,47 @@
-﻿using System.Linq;
-using System.Reflection;
-using NSpectator;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NSpectator.Domain;
 using NSpectator.Domain.Formatters;
 using FluentAssertions;
+using NSpectator;
+// ReSharper disable once CheckNamespace
 
-/*
- * Howdy,
- * 
- * This is NSpectator's DebuggerShim.  It will allow you to use Resharper's test runner to run
- * NSpectator tests that are in the same Assembly as this class.
- */
-
-//[TestFixture]
-public class DebuggerShim
+/// <summary>
+/// Howdy,
+/// 
+/// <para>NSpectator's DebuggerShim allow you to use Resharper's test runner to run specs that are in the same Assembly as this class.</para>
+/// <para>You can also inherit your Program class from this class and explicitly call Debug or wrap it into any unit test.</para>
+/// <para>For more information visit https://github.com/nspectator/NSpectator/wiki </para>
+/// </summary>
+public partial class DebuggerShim
 {
-    //[Test]
-    public void Debug()
+    public void Debug(string tagOrClassName)
     {
-        var tagOrClassName = "class_or_tag_you_want_to_debug";
-
-        var types = GetType().Assembly.GetTypes(); 
+        var types = GetType().Assembly.GetTypes();
         // OR
         // var types = new Type[]{typeof(Some_Type_Containg_some_Specs)};
+        Debug(new SpecFinder(types, ""));
+    }
 
-        var finder = new SpecFinder(types, "");
+    public static void Debug(System.Type t)
+    {
+        Debug(new SpecFinder(new[] { t }, ""));
+    }
 
-        var tagsFilter = new Tags().Parse(tagOrClassName);
+    public static void Debug(IEnumerable<System.Type> types)
+    {
+        Debug(new SpecFinder(types.ToArray(), ""));
+    }
 
-        var builder = new ContextBuilder(finder, tagsFilter, new DefaultConventions());
-
-        var runner = new ContextRunner(tagsFilter, new ConsoleFormatter(), false);
-
+    private static void Debug(SpecFinder finder)
+    {
+        var builder = new ContextBuilder(finder, new DefaultConventions());
+        var runner = new ContextRunner(new Tags(), new ConsoleFormatter(), false);
         var results = runner.Run(builder.Contexts().Build());
 
-        //assert that there aren't any failures
-        results.Failures().Count().Should().Be(0);
+        // assert that there aren't any failures
+        results.Failures().Count().Should().Be(0, "all examples passed");
     }
 }
+
+
