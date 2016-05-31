9489bed622a31bc41d3a59ebf4fff7cc6dd81f47
Ivan Novikov <novikov@amiforus.com>
2016-05-31 22:00:40 +0300

    Preparing for next major version.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -21,5 +21,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyFileVersion("1.0.4")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
